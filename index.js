2c5d63c6adfaddb78e1579e53f59de2ae3ddc657
unknown <sudikshyarajkarnikar@gmail.com>
2022-07-04 20:06:01 +0545

    add function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('git');
+let primarySum=0,secondarySum = 0,matrix=[[1,1,1],[1,2,3],[10,2,3]];
+calc = (type)=>{
+ 
+    for (let i=0;i<matrix.length;i++){
+       primarySum += matrix[i][i];
+       secondarySum+= matrix[i][matrix.length-i-1];
+    }
+    if(type =='primary'){
+        return(primarySum);
+    }
+    else if(type == 'secondary' ){
+        return(secondarySum);
+    }
+    else{
+        return 'invalid';
+    }
+}
+ 
+result = calc('primary');
